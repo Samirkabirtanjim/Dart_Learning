4569f849a8faabb094ac4dfea38435ebedc0b36e
Samir Kabir Tanjim <122144139+Samirkabirtanjim@users.noreply.github.com>
2025-11-02 12:31:35 +0600

    Implement Animal class with speak method

--- Module 2: Advanced topics in Dart/OOP_Concept_and_Class.dart
+++ Module 2: Advanced topics in Dart/OOP_Concept_and_Class.dart
@@ -1,1 +1,17 @@
+// Create a class named Animal that has:
+// a variable name
+// a function speak() that prints "The animal makes a sound"
+// Then, in main(), create one object of Animal and call the speak() function.
 
+void main() {
+    Animal dog = Animal();
+    dog.name = "Dog";
+    dog.speak();
+}
+
+class Animal{
+    String? name;
+    void speak(){
+        print("The animal makes a sound $name");
+    }
+}
