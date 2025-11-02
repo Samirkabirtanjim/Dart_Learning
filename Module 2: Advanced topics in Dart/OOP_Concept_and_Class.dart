d89334fafa1e6984b01a40bf476197706b6bdc08
Samir Kabir Tanjim <122144139+Samirkabirtanjim@users.noreply.github.com>
2025-11-02 12:35:40 +0600

    Fix method name and improve print statements

--- Module 2: Advanced topics in Dart/OOP_Concept_and_Class.dart
+++ Module 2: Advanced topics in Dart/OOP_Concept_and_Class.dart
@@ -1,3 +1,4 @@
+// 🧮 Question 1:
 // Create a class named Animal that has:
 // a variable name
 // a function speak() that prints "The animal makes a sound"
@@ -13,5 +14,29 @@ class Animal{
     String? name;
     void speak(){
         print("The animal makes a sound $name");
+    }
+}
+
+// 🚗 Question 2:
+// Create a class Car with:
+// variables brand and year
+// a method showInfo() that prints:
+// "Brand: <brand>, Year: <year>"
+// Then, create an object of Car in main() and show its info.
+
+void main(){
+    Car mycar = Car();
+    mycar.brand = "BMW";
+    mycar.year = 2026;
+    
+    mycar.showinfo();
+}
+
+class Car{
+    String? brand;
+    int? year;
+    
+    showinfo(){
+        print("Car -> Brand; $brand & Year: $year");
     }
 }
