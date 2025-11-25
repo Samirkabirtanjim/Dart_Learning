93cdfb0465f1190aceca562157d2e3d4655284b6
Samir Kabir Tanjim <122144139+Samirkabirtanjim@users.noreply.github.com>
2025-11-25 21:09:56 +0600

    Implement function to remove special characters from input

--- Module 3: List of Beginner problems/Remove_Special_Character.dart
+++ Module 3: List of Beginner problems/Remove_Special_Character.dart
@@ -1,1 +1,20 @@
+import 'dart:io';
 
+String removeSpecialChar(String input){
+  String result = "";
+  List<String> specialChars = ['.', ',', '!', '@', '#', r'$', '%', '^', '&', '*', '(', ')'];
+  for(int i = 0; i < input.length; i++){
+    if(!specialChars.contains(input[i])){
+      result += input[i];
+    }
+  }
+  return result;
+}
+
+void main(){
+  String? input = stdin.readLineSync();
+  if(input != null){
+    String cleaned = removeSpecialChar(input);
+    print(cleaned);
+  }
+}
