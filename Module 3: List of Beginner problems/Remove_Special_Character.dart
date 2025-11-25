9a2edaacda2a7570160fb4450c9e1ee524374871
Samir Kabir Tanjim <122144139+Samirkabirtanjim@users.noreply.github.com>
2025-11-25 21:19:37 +0600

    Enhance removeSpecialChar function for clarity

--- Module 3: List of Beginner problems/Remove_Special_Character.dart
+++ Module 3: List of Beginner problems/Remove_Special_Character.dart
@@ -1,20 +1,45 @@
+/*
+Problem Statement:
+Write a program to create a function that takes a string,
+removes all "special" characters (., !, @, #, $, %, ^, &, *, (, )),
+and returns the new string.
+
+Input:
+The program will take a string S.
+
+Output:
+The output will print a string without any special characters.
+
+Constraints:
+0 ≤ |S| ≤ 1000
+Input special characters will be from (., !, @, #, $, %, ^, &, *, (, ))
+
+Example:
+Enter string: ab!cd
+Output: abcd
+*/
+
 import 'dart:io';
 
-String removeSpecialChar(String input){
+// Function to remove special characters
+String removeSpecialChar(String input) {
   String result = "";
   List<String> specialChars = ['.', ',', '!', '@', '#', r'$', '%', '^', '&', '*', '(', ')'];
-  for(int i = 0; i < input.length; i++){
-    if(!specialChars.contains(input[i])){
+  
+  for (int i = 0; i < input.length; i++) {
+    if (!specialChars.contains(input[i])) {
       result += input[i];
     }
   }
   return result;
 }
 
-void main(){
+void main() {
+  stdout.write("Enter string: ");
   String? input = stdin.readLineSync();
-  if(input != null){
+  
+  if (input != null) {
     String cleaned = removeSpecialChar(input);
-    print(cleaned);
+    print("Output: $cleaned");
   }
 }
